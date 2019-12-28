018c8b9e9c896a381ebf7683fdec62b2f9aaf888
WaltRibeiro <walt@fororchestra.com>
2019-12-28 00:59:23 -0500

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// https://teamtreehouse.com/library/useful-array-methods
+
+var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
+var search;
+
+function print(message) {
+  document.write( '<p>' + message + '</p>');
+}
+
+while (true) {
+    search = prompt("Search for a product in our store. Type 'list' to show all of the produce and 'quit' to exit");
+      if (search === 'quit') {
+       break;   
+      } else if (search === 'list') {
+        print();
+      }
+}
